c82dc2eaa2481ebb79c4997e4a2398b347b49f17
mdclaymont@gmail.com <mkarim010@gmail.com>
2020-04-27 13:05:21 -0400

    This is first maven Test

---
+++ src/test/java/Criglist/TestClass1.java
@@ -0,0 +1,28 @@
+package Criglist;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	
+	@Test
+	public void a1()
+	{
+		System.out.println("This is testtt a1");
+	}
+	@Test
+	public void a2()
+	{
+		System.out.println("This is testtt a2");
+	}
+	@Test
+	public void a3()
+	{
+		System.out.println("This is testtt a3");
+	}
+	@Test
+	public void a4()
+	{
+		System.out.println("This is testtt a4");
+	}
+
+}
